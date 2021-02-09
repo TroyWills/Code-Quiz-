3b7500c28580cff5d9dde98d0dc81c91979a8519
Troy Wills <troy_wills@outlook.com>
2021-02-08 22:44:29 -0600

    progress

---
+++ questions.js
@@ -0,0 +1,46 @@
+// list of all questions, choices, and answers
+var questions = [
+    {
+        title: "An array starts at index of 0" 
+        choices: [
+            "True"
+            "False"
+        ]
+        answer: "True" 
+    },
+    {
+        title: "Math.Random() will return:"
+        choices: [
+            "A number less than 1"
+            "A number greater than 1"
+            "Any random integer"
+            "Any random number"
+        ]
+        answer: "A number less than 1"
+    },
+    {
+        title: "While using a switch statement, the ___ will stop the block of code from being executed"
+        choices: [
+            "break"
+            "return"
+            "for-loop"
+            "boolean"
+        ]
+        answer: "break"
+    },
+    {
+        title:
+        choices:
+        answer
+    },
+    {
+        title:
+        choices:
+        answer
+    },
+    {
+        title:
+        choices:
+        answer
+    },
+]
