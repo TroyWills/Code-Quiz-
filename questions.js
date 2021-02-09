201da27f9197bfe4d29abe5b4252390d32384e6d
Troy Wills <troy_wills@outlook.com>
2021-02-09 10:13:42 -0600

    savce

--- questions.js
+++ questions.js
@@ -29,18 +29,22 @@ var questions = [
         answer: "break"
     },
     {
-        title:
-        choices:
-        answer
+        title: "A JavaScript for-loop is effective if you want to"
+        choices: [
+            "Run the same block of code over and over again"
+            "Compare values"
+            "Return an object when a button is clicked"
+            "Style a webpage"
+        ]
+        answer: "Run the same block of code over and over again"
     },
     {
-        title:
-        choices:
-        answer
-    },
-    {
-        title:
-        choices:
-        answer
+        title: "JavaScript objects:"
+        choices: [
+            "are variables"
+            "Store many values"
+            "Are stored inside of curly brackets"
+            "All of the above"
+        answer: "All of the above"
     },
 ]
