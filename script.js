406ecb0f1b1fac0164d3c9ce98ddd97cef046422
Troy Wills <you@example.com>
2021-02-23 23:30:41 -0600

    save

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // variables for elements
-var startButton = document.querySelector("#quizBtn")
-var questionsDiv = document.getElementById("questions")
+var startButton = document.querySelector("#quizBtn");
+var questionsDiv = document.getElementById("questions");
+var choicesList = document.getElementById("choicesList");
+var questionTitle = document.getElementById("title");
+
+// store answers in an empty array
+var userInput = [];
+
+// variable to classify first question 
+var questionNumber = 0
+
 var questions = [
     {
         title: "An array starts at index of 0",
@@ -52,39 +61,56 @@ var questions = [
     },
 ]
 
+// write a function that grabs the current question at index 0
+function grabQuestion(){
+    // variable that will grab the current question in the array we are working on
+    var currentQuestion = questions[questionNumber];
+    
+    // create array to hold answers 
+    let answers = [];
+
+    answers = currentQuestion.choices;
+
+    // print the title of the current question to the h2
+    questionTitle.innerText = currentQuestion.title;
+    // print the choices to the choiceList variable
+    // choicesList.innerText = currentQuestion.choices;
+    // use map method to wrap each answer in button
+    let answersHTML = answers.map(answer => `<button class = "button">${answer}</button>`);
+    // write the possible answers to the UI wrapped in button
+    choicesList.innerHTML = answersHTML;
+    // use event delegation to handle dynamic HTML button as any click in the body advances to next question
+    document.addEventListener("click", function(e){
+        console.log(e.target);
+        // if user does not click on button do not proceed to next question
+        // if = e.target.innerText 
+        if (e.target.innerText === currentQuestion.answer){
+            console.log("Correct!");
+            answers.push("Correct");
+            console.log(answers);
+        }
+        else{
+            console.log("Wrong!");
+        }
+        // // push the targeted element into the answers array in global variables
+        // else (e.target = button)
+        //     // if e.target = button move onto next question
+    })
+}
+
 // function that runs quiz questions
 function startQuiz(){
-// hide the start screen
-var startScreen = document.getElementById("startScreen");
-startScreen.setAttribute("class","hide");
-questionsDiv.removeAttribute("class");
 
-    // what question we are going to print to the UI
-    // targeting the H2 element for title to print 
-    // targeting the UL element for choices to print 
-    // print the question to the targeted H2 *since the questions is a variable do not have to make a string* <-- when referring to textContent documentation
+    // input timer & display (set interval)
 
- for (i = 0; i < questions.length; i++) {
-    var currentQuestion = questions[i];
-    var title = document.getElementById("title");
-    var choiceBox = document.getElementById("choices");
-    var choices = [];
-    title.textContent = currentQuestion.title;
+    // hide the start screen
+    var startScreen = document.getElementById("startScreen");
+    startScreen.setAttribute("class","hide");
 
-    // choices.innerHTML = "";  <-- clears any prior HTML content 
-    // create another for loop to display the questions as selectable buttons
-
-    for (i = 0; i < currentQuestion.choices.length; i++) {
-        choices.push(currentQuestion.choices[i]);
-    }
-        console.log(choices);
- } 
-
-// input timer & display (set interval)
-};  
+    // remove the hide class to display questions
+    questionsDiv.removeAttribute("class");
+    grabQuestion();
+ };  
 
 // starts quiz when button is clicked 
 startButton.addEventListener("click", startQuiz);
-
-
-
